022d0eb397a8c2204f27a0f6388345acc917c19a
safaltagautam <safaltagautam1@gmail.com>
2026-04-17 12:13:04 +0545

    safalta gautam

---
+++ safalta/areatype.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class areatype {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter length: ");
+        double length = sc.nextDouble();
+
+        System.out.print("Enter breadth: ");
+        double breadth = sc.nextDouble();
+
+        double area = length * breadth;
+
+        int areaInt = (int) area;   
+
+        System.out.println("Area of rectangle (int) = " + areaInt);
+
+        sc.close();
+
+       
+    }
+    
+}
